c64e0019f0c0690e66d6b90dcd951eb121cc6251
veliknon <veliknon@gmail.com>
2019-03-02 14:43:18 +0300

    initial commit

---
+++ app/src/main/java/com/example/intentapp/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.intentapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_SEND);
+                intent.setType("text/plain");
+                intent.putExtra(Intent.EXTRA_TEXT, getString(R.string.message));
+                Intent chosenIntent = Intent.createChooser(intent, "Заголовок в диалоговом окне");
+                startActivity(chosenIntent);
+            }
+        });
+    }
+}
